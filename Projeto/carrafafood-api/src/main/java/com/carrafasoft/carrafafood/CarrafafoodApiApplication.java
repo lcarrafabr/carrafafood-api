74f476caf1dc377eb736b852affa598d3a954176
lcarrafabr <lcarrafa.br@gmail.com>
2022-11-11 11:30:17 -0300

    Criando o projeto

---
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/CarrafafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.carrafasoft.carrafafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrafafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrafafoodApiApplication.class, args);
+	}
+
+}
---
+++ Projeto/carrafafood-api/src/test/java/com/carrafasoft/carrafafood/CarrafafoodApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carrafasoft.carrafafood;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class CarrafafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
