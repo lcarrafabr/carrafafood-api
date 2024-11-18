ecf7ba938ca74af7ae121663b11ab15f3c860741
LCarrafaBR <lcarrafa.br@gmail.com>
2024-11-18 16:02:35 -0300

    Lançando exception de desserialização na atualização PATCH

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteController.java
@@ -7,10 +7,14 @@ import java.util.Map;
 import java.util.Optional;
 
 import com.carrafasoft.carrafafood.domain.exception.NegocioException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -27,6 +31,8 @@ import com.carrafasoft.carrafafood.domain.repository.RestauranteRepository;
 import com.carrafasoft.carrafafood.domain.service.CadastroRestauranteService;
 import com.carrafasoft.carrafafood.infrastructure.repository.spec.RestauranteSpecs;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -73,35 +79,42 @@ public class RestauranteController {
 			throw new NegocioException(e.getMessage());
 		}
 	}
-//
-//	@PatchMapping("{restauranteId}")
-//	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
-//
-//		Optional<Restaurante> restauranteAtual = restauranteRepository.findById(restauranteId);
-//
-//		if(!restauranteAtual.isPresent()) {
-//			return ResponseEntity.notFound().build();
-//		}
-//
-//		merge(campos, restauranteAtual.get());
-//
-//		return atualizar(restauranteId, restauranteAtual.get());
-//	}
 
-	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
-		
-		ObjectMapper objectMapper = new ObjectMapper();
-		Restaurante restauranteOrigem  = objectMapper.convertValue(camposOrigem, Restaurante.class);
-		
-		camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-			field.setAccessible(true);
-			
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-			
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
+	@PatchMapping("{restauranteId}")
+	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos,
+										HttpServletRequest request) {
+
+		Restaurante restauranteAtual = restauranteService.buscarOuFalhar(restauranteId);
+
+		merge(campos, restauranteAtual, request);
+
+		return atualizar(restauranteId, restauranteAtual);
+	}
+
+	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+
+		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
+
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+
+			Restaurante restauranteOrigem  = objectMapper.convertValue(camposOrigem, Restaurante.class);
+
+			camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				field.setAccessible(true);
+
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException e) {
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+		}
 	}
 	
 	@GetMapping("/busca-nome-frete")
