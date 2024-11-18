47c95d457c123414e0c19518bf5792f299769dc5
LCarrafaBR <lcarrafa.br@gmail.com>
2024-11-18 12:00:36 -0300

    tratando a exception InvalidFormatException na desserialização

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/exceptionhandler/ApiExceptionHandler.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,8 @@ package com.carrafasoft.carrafafood.api.exceptionhandler;
 import com.carrafasoft.carrafafood.domain.exception.EntidadeEmUsoException;
 import com.carrafasoft.carrafafood.domain.exception.EntidadeNaoEncontradaException;
 import com.carrafasoft.carrafafood.domain.exception.NegocioException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +15,22 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final String CORPO_DA_REQUISICAO_INVALIDO = "O Corpo da requisição está inválido. Verifique erro de sintaxe.";
+    private static final String PROPRIEDADE_COM_VALOR_INVALIDO_ERRO = "A propriedade '%s' recebeu o valor '%s' que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %S.";
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if(rootCause instanceof InvalidFormatException) {
+            return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+        }
 
         ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
         String detail = CORPO_DA_REQUISICAO_INVALIDO;
@@ -102,5 +112,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 .title(problemType.getTitle())
                 .detail(detail)
                 .dataHora(LocalDateTime.now());
+    }
+
+    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        String path = ex.getPath().stream()
+                .map(ref -> ref.getFieldName())
+                .collect(Collectors.joining("."));
+
+        String detail = String.format(PROPRIEDADE_COM_VALOR_INVALIDO_ERRO, path, ex.getValue(), ex.getTargetType().getSimpleName());
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
     }
 }
