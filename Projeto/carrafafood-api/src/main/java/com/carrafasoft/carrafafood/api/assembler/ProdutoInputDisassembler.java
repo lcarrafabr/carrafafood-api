c4e30a3f177a8494017e33f9acd539fbc554cf46
LCarrafaBR <lcarrafa.br@gmail.com>
2025-01-06 12:00:59 -0300

    Desafio implementando endpoint de produtos

---
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/assembler/ProdutoInputDisassembler.java
@@ -0,0 +1,22 @@
+package com.carrafasoft.carrafafood.api.assembler;
+
+import com.carrafasoft.carrafafood.api.model.input.ProdutoInput;
+import com.carrafasoft.carrafafood.domain.model.Produto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Produto toDomainObject(ProdutoInput produtoInput) {
+        return modelMapper.map(produtoInput, Produto.class);
+    }
+
+    public void copyToDomainObject(ProdutoInput produtoInput, Produto produto) {
+        modelMapper.map(produtoInput, produto);
+    }
+}
---
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/domain/service/CadastroProdutoService.java
@@ -0,0 +1,25 @@
+package com.carrafasoft.carrafafood.domain.service;
+
+import com.carrafasoft.carrafafood.domain.exception.ProdutoNaoEncontradoException;
+import com.carrafasoft.carrafafood.domain.model.Produto;
+import com.carrafasoft.carrafafood.domain.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CadastroProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Transactional
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto buscarOuFalhar(Long restauranteId, Long produtoId) {
+        return produtoRepository.findById(restauranteId, produtoId)
+                .orElseThrow(() -> new ProdutoNaoEncontradoException(restauranteId, produtoId));
+    }
+}
