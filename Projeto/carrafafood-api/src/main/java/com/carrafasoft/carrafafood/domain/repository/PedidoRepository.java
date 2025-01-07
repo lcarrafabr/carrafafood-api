3d3d8e6226235520df3231279cff7c9ffe47cf46
LCarrafaBR <lcarrafa.br@gmail.com>
2025-01-07 12:26:14 -0300

    Usando IDs vs UUIDs nas URIs de recursos

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/domain/repository/PedidoRepository.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/domain/repository/PedidoRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PedidoRepository extends CustomJpaRepository<Pedido, Long> {
 
+    //@Query("from Pedido where codigo = :codigo")
+    Optional<Pedido> findByCodigo(String codigo);
 
 //    @Query(nativeQuery = true,
 //    value = "Select * from pedido p join restaurante r on p.id = r.id")
