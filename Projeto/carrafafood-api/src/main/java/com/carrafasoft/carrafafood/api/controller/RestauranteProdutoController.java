9ba0cf240c8a72d45b80b9954c105da4bce5a9d3
LCarrafaBR <lcarrafa.br@gmail.com>
2025-01-12 17:39:39 -0300

    implemenmtando pesquisa simples na API

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteProdutoController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteProdutoController.java
@@ -36,10 +36,16 @@ public class RestauranteProdutoController {
     private ProdutoInputDisassembler produtoInputDisassembler;
 
     @GetMapping
-    public List<ProdutoModel> listar(@PathVariable Long restauranteId) {
+    public List<ProdutoModel> listar(@PathVariable Long restauranteId,
+                                     @RequestParam(required = false) boolean incluirInativos) {
         Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
+        List<Produto> todosProdutos = null;
 
-        List<Produto> todosProdutos = produtoRepository.findByRestaurante(restaurante);
+        if(incluirInativos) {
+            todosProdutos = produtoRepository.findTodosByRestaurante(restaurante);
+        } else {
+            todosProdutos = produtoRepository.findAtivosByRestaurante(restaurante);
+        }
 
         return produtoModelAssembler.toCollectionModel(todosProdutos);
     }
