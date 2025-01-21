d91d975cd99359362043ebd67fef16c8c76d6ba5
LCarrafaBR <lcarrafa.br@gmail.com>
2025-01-21 15:31:41 -0300

    Implamentando upload de arquivo com Multipart/form-data

---
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,38 @@
+package com.carrafasoft.carrafafood.api.controller;
+
+import com.carrafasoft.carrafafood.api.model.input.FotoProdutoInput;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId,
+                              @PathVariable Long produtoId,
+                              FotoProdutoInput fotoProdutoInput) {
+
+        try {
+            var nomeArquivo = UUID.randomUUID().toString()
+                    + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+
+            var arquivoFoto = Path.of("E:/PROJETOS/Algaworks/Especialista_Spring_Rest/catalogo", nomeArquivo);
+
+            System.out.println(fotoProdutoInput.getDescricao());
+            System.out.println(arquivoFoto);
+            System.out.println(fotoProdutoInput.getArquivo().getContentType());
+
+            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/model/input/FotoProdutoInput.java
@@ -0,0 +1,13 @@
+package com.carrafasoft.carrafafood.api.model.input;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+public class FotoProdutoInput {
+
+    private MultipartFile arquivo;
+    private String descricao;
+}
