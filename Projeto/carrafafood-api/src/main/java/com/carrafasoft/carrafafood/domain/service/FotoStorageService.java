46d908f66e98b30ed961b15dfc2b646fd104365b
LCarrafaBR <lcarrafa.br@gmail.com>
2025-01-22 08:54:28 -0300

    Implementando o serviço de armazenagem de fotos no disco local

---
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/domain/service/FotoStorageService.java
@@ -0,0 +1,19 @@
+package com.carrafasoft.carrafafood.domain.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.InputStream;
+
+public interface FotoStorageService {
+
+    void armazenar(NovaFoto novaFoto);
+
+    @Builder
+    @Getter
+    class NovaFoto {
+
+        private String nomeArquivo;
+        private InputStream inputStream;
+    }
+}
---
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -0,0 +1,36 @@
+package com.carrafasoft.carrafafood.infrastructure.service.storage;
+
+import com.carrafasoft.carrafafood.domain.service.FotoStorageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class LocalFotoStorageService implements FotoStorageService {
+
+    @Value("${carrafafood.storage.local.diretorio-fotos}")
+    private Path diretorioFotos;
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+
+        try {
+
+            Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
+
+            FileCopyUtils.copy(novaFoto.getInputStream(),
+                    Files.newOutputStream(arquivoPath));
+
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível armazenar o arquivo.", e);
+        }
+    }
+
+    private Path getArquivoPath(String nomeArquivo) {
+        return diretorioFotos.resolve(Path.of(nomeArquivo));
+    }
+}
