d63701a808988a7b3c75ffa9388c4f803d6187c8
LCarrafaBR <lcarrafa.br@gmail.com>
2025-01-22 06:09:25 -0300

    Validado ocntent type do arquivo - criando anotação @FileContentType

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/model/input/FotoProdutoInput.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/model/input/FotoProdutoInput.java
@@ -1,8 +1,10 @@
 package com.carrafasoft.carrafafood.api.model.input;
 
+import com.carrafasoft.carrafafood.core.validation.FileContentType;
 import com.carrafasoft.carrafafood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
@@ -14,6 +16,7 @@ public class FotoProdutoInput {
 
     @NotNull
     @FileSize(max = "1MB")
+    @FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
     private MultipartFile arquivo;
 
     @NotBlank
