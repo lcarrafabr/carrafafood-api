263d4c4e1cfc416447c902f578ba56529baeffc3
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-07 14:25:28 -0300

    Descrevendo restrições de validação de propriedades do modelo

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/model/dto/CidadeModel.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/model/dto/CidadeModel.java
@@ -1,5 +1,6 @@
 package com.carrafasoft.carrafafood.api.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,10 @@ import lombok.Setter;
 @Getter
 public class CidadeModel {
 
+    @ApiModelProperty(value = "ID da cidade", example = "1", required = true)
     private Long id;
+
+    @ApiModelProperty(example = "São Paulo", required = true)
     private String nome;
     private EstadoModel estado;
 
--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/model/dto/EstadoModel.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/model/dto/EstadoModel.java
@@ -1,5 +1,6 @@
 package com.carrafasoft.carrafafood.api.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,10 @@ import lombok.Setter;
 @Getter
 public class EstadoModel {
 
+    @ApiModelProperty(example = "1", required = true)
     private Long id;
+
+    @ApiModelProperty(example = "São Caetano do Sul", required = true)
     private String nome;
 
 }
