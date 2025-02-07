98d54e48baf6ffe8a9a24d7d8ec85cf8ccf45209
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-07 10:58:41 -0300

    Descrevendo informações da API na documentação

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/core/openapi/SpringfoxConfig.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/core/openapi/SpringfoxConfig.java
@@ -2,7 +2,10 @@ package com.carrafasoft.carrafafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,6 +18,17 @@ public class SpringfoxConfig {
         return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.carrafasoft.carrafafood.api"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("CarrafaFood API")
+                .description("Api de estudos da Algaworks para clientes e restaurantes")
+                .version("1.0")
+                .contact(new Contact("CarrafaFood", "http://localhost:8080", "lcarrafa.br@gmail.com"))
                 .build();
     }
 }
