1c36e5ab549e395abfee5e1b54702a4a2c5f7391
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-10 20:38:17 -0300

    Descrevendo media type da resposta nos endpoints

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/CidadeController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/CidadeController.java
@@ -16,13 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cidades")
+@RequestMapping(path = "/cidades", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CidadeController implements CidadeControllerOpenApi {
 
 	public static final String CIDADE_EM_USO = "Cidade de código %d não pode ser removida, pois está em uso.";
