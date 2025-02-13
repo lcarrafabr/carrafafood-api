8f45d44cdc9bb3e9a70066e0a47a8356adb5a784
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-13 17:36:58 -0300

    Desafio: descrevendo documentação de endpoints de associação de restaurantes com formas de pagamento

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteFormaPagamentoController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteFormaPagamentoController.java
@@ -2,17 +2,19 @@ package com.carrafasoft.carrafafood.api.controller;
 
 import com.carrafasoft.carrafafood.api.assembler.FormaPagamentoModelAssembler;
 import com.carrafasoft.carrafafood.api.model.dto.FormaPagamentoModel;
+import com.carrafasoft.carrafafood.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
 import com.carrafasoft.carrafafood.domain.model.Restaurante;
 import com.carrafasoft.carrafafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/restaurantes/{restauranteId}/formas-pagamento")
-public class RestauranteFormaPagamentoController {
+@RequestMapping(path = "/restaurantes/{restauranteId}/formas-pagamento", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 
 	
 	@Autowired
