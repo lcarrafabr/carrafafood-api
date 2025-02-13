45c6ffd702fd414215224209dccd6bd15b98368c
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-13 17:26:53 -0300

    Desafio: descrevendo documentação de endpoints de fluxo de pedidos

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/FluxoPedidoController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/FluxoPedidoController.java
@@ -1,13 +1,15 @@
 package com.carrafasoft.carrafafood.api.controller;
 
+import com.carrafasoft.carrafafood.api.openapi.controller.FluxoPedidoControllerOpenApi;
 import com.carrafasoft.carrafafood.domain.service.FluxoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/pedidos/{codigoPedido}")
-public class FluxoPedidoController {
+@RequestMapping(path = "/pedidos/{codigoPedido}", produces = MediaType.APPLICATION_JSON_VALUE)
+public class FluxoPedidoController implements FluxoPedidoControllerOpenApi {
 
     @Autowired
     private FluxoPedidoService fluxoPedido;
