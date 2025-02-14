f0b6f35bf4c1dfeabb712c6d72edbaa17c064b14
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-14 06:23:00 -0300

    Servindo arquivos de fotos pela API

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteProdutoFotoController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/RestauranteProdutoFotoController.java
@@ -3,17 +3,22 @@ package com.carrafasoft.carrafafood.api.controller;
 import com.carrafasoft.carrafafood.api.assembler.FotoProdutoModelAssembler;
 import com.carrafasoft.carrafafood.api.model.dto.FotoProdutoModel;
 import com.carrafasoft.carrafafood.api.model.input.FotoProdutoInput;
+import com.carrafasoft.carrafafood.domain.exception.EntidadeNaoEncontradaException;
 import com.carrafasoft.carrafafood.domain.model.FotoProduto;
 import com.carrafasoft.carrafafood.domain.model.Produto;
 import com.carrafasoft.carrafafood.domain.service.CadastroProdutoService;
 import com.carrafasoft.carrafafood.domain.service.CatalogoFotoProdutoService;
+import com.carrafasoft.carrafafood.domain.service.FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.UUID;
 
@@ -29,6 +34,9 @@ public class RestauranteProdutoFotoController {
     private FotoProdutoModelAssembler fotoProdutoModelAssembler;
     @Autowired
     private CatalogoFotoProdutoService catalogoFotoProdutoService;
+
+    @Autowired
+    private FotoStorageService fotoStorage;
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId,
@@ -52,11 +60,27 @@ public class RestauranteProdutoFotoController {
 
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public FotoProdutoModel buscar(@PathVariable Long restauranteId,
                                    @PathVariable Long produtoId) {
         FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
 
         return fotoProdutoModelAssembler.toModel(fotoProduto);
+    }
+
+    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId,
+                                                          @PathVariable Long produtoId) {
+        try {
+            FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+
+            InputStream inputStream = fotoStorage.recuperar(fotoProduto.getNomeArquivo());
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(new InputStreamResource(inputStream));
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
