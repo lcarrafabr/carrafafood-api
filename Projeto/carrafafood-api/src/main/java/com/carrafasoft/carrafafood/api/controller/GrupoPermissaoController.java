af7fa9706e8c204ba216469ccc513a1d1ccbf847
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-14 10:24:45 -0300

    Desafio: descrevendo documentação de endpoints de associação de permissões com grupos

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/GrupoPermissaoController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/GrupoPermissaoController.java
@@ -1,18 +1,20 @@
 package com.carrafasoft.carrafafood.api.controller;
 
 import com.carrafasoft.carrafafood.api.assembler.PermissaoModelAssembler;
+import com.carrafasoft.carrafafood.api.openapi.controller.GrupoPermissaoControllerOpenApi;
 import com.carrafasoft.carrafafood.domain.model.Grupo;
 import com.carrafasoft.carrafafood.domain.model.PermissaoModel;
 import com.carrafasoft.carrafafood.domain.service.CadastroGrupoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/grupos/{grupoId}/permissoes")
-public class GrupoPermissaoController {
+@RequestMapping(path = "/grupos/{grupoId}/permissoes", produces = MediaType.APPLICATION_JSON_VALUE)
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi {
 
     @Autowired
     private CadastroGrupoService cadastroGrupo;
