ddce45cee5743e3ac4947dc8e1b6334bb36e0e88
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-14 10:36:47 -0300

    Desafio: descrevendo documentação de endpoints de associação de grupos com usuários

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/UsuarioGrupoController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/controller/UsuarioGrupoController.java
@@ -2,17 +2,19 @@ package com.carrafasoft.carrafafood.api.controller;
 
 import com.carrafasoft.carrafafood.api.assembler.GrupoModelAssembler;
 import com.carrafasoft.carrafafood.api.model.dto.GrupoModel;
+import com.carrafasoft.carrafafood.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.carrafasoft.carrafafood.domain.model.Usuario;
 import com.carrafasoft.carrafafood.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(path = "/usuarios/{usuarioId}/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     private CadastroUsuarioService cadastroUsuario;
