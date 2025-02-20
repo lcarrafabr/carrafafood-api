15ef702f6fa40abd03cd1e265c6f1db2a8720cb1
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-20 16:05:42 -0300

    Desafio: adicionando template variables do filtro de pedidos

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/assembler/PedidoModelAssembler.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/assembler/PedidoModelAssembler.java
@@ -36,9 +36,15 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
                 new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM)
         );
 
+        TemplateVariables filtroVariables = new TemplateVariables(
+                new TemplateVariable("clienteId", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("restauranteId", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoInicio", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoFim", TemplateVariable.VariableType.REQUEST_PARAM));
+
         String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
 
-        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl,pageVariables), "pedidos"));
+        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl,pageVariables.concat(filtroVariables)), "pedidos"));
 
         pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
                 .buscar(pedido.getRestaurante().getId())).withSelfRel());
