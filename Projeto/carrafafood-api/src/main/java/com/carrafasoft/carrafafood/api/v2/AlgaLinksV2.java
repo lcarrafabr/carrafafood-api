2e28a680ef4e5763d2635476279c3fbc4fe0a818
LCarrafaBR <lcarrafa.br@gmail.com>
2025-02-25 21:11:00 -0300

    Desafio: adicionando os recursos de cozinhas na v2 da API

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/v2/AlgaLinksV2.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/v2/AlgaLinksV2.java
@@ -2,6 +2,7 @@ package com.carrafasoft.carrafafood.api.v2;
 
 import com.carrafasoft.carrafafood.api.v1.controller.*;
 import com.carrafasoft.carrafafood.api.v2.controller.CidadeControllerV2;
+import com.carrafasoft.carrafafood.api.v2.controller.CozinhaControllerV2;
 import org.springframework.hateoas.*;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,14 @@ public class AlgaLinksV2 {
 
     public Link linkToCidades() {
         return linkToCidades(IanaLinkRelations.SELF.value());
+    }
+
+    public Link linkToCozinhas(String rel) {
+        return linkTo(CozinhaControllerV2.class).withRel(rel);
+    }
+
+    public Link linkToCozinhas() {
+        return linkToCozinhas(IanaLinkRelations.SELF.value());
     }
 
 }
