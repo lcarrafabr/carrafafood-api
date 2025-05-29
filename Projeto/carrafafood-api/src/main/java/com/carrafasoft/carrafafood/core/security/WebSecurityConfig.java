69ebb186d1ea83d00724be41d31673bb45652c14
LCarrafaBR <lcarrafa.br@gmail.com>
2025-05-29 15:15:36 -0300

    Configurando autenticação de usuários em memória

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/core/security/WebSecurityConfig.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/core/security/WebSecurityConfig.java
@@ -1,15 +1,32 @@
 package com.carrafasoft.carrafafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("lcarrafa")
+                    .password(passwordEncoder().encode("123456"))
+                    .roles("ADMIN")
+                .and()
+                .withUser("debora")
+                    .password(passwordEncoder().encode("123"))
+                    .roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,5 +43,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
     }
 }
