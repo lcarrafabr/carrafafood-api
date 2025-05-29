fc1d26bfff9019e06d0a71cf6d758b45f751311a
LCarrafaBR <lcarrafa.br@gmail.com>
2025-05-29 20:51:07 -0300

    Criando o projeto do Authorization Server com Spring Security OAuth2

---
+++ Projeto/carrafafood-auth/src/main/java/com/carrafafood/carrafafood/auth/CarrafafoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.carrafafood.carrafafood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrafafoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrafafoodAuthApplication.class, args);
+	}
+
+}
---
+++ Projeto/carrafafood-auth/src/test/java/com/carrafafood/carrafafood/auth/CarrafafoodAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrafafood.carrafafood.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrafafoodAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
