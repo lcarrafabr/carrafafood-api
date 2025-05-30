593a69c9ebcef4cbebc2b133b2f2ef1d05ef2ffa
LCarrafaBR <lcarrafa.br@gmail.com>
2025-05-29 21:21:41 -0300

    Configurando o fluxo Authorization Server com Password Credentials e Opaque Tokens

--- Projeto/carrafafood-auth/src/main/java/com/carrafafood/carrafafood/auth/WebSecurityConfig.java
+++ Projeto/carrafafood-auth/src/main/java/com/carrafafood/carrafafood/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.carrafafood.carrafafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,5 +33,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
