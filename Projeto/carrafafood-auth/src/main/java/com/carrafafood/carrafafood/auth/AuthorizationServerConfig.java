80582fb7d020e5f1b94dc0b1c01547f0690be0ad
LCarrafaBR <lcarrafa.br@gmail.com>
2025-05-30 21:32:21 -0300

    Configurando o Client Credentials Grant Type no Authorization Server

--- Projeto/carrafafood-auth/src/main/java/com/carrafafood/carrafafood/auth/AuthorizationServerConfig.java
+++ Projeto/carrafafood-auth/src/main/java/com/carrafafood/carrafafood/auth/AuthorizationServerConfig.java
@@ -33,7 +33,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                     .authorizedGrantTypes("password", "refresh_token")
                     .scopes("write", "read")
                 .accessTokenValiditySeconds(60 * 60 * 6)
-                .refreshTokenValiditySeconds(60* 60);
+                .refreshTokenValiditySeconds(60* 60)
+
+                //criando um grant_tipe simulando um outro backend pra se conectar a API
+                .and()
+                .withClient("faturamento")
+                    .secret(passwordEncoder.encode("faturamento123"))
+                    .authorizedGrantTypes("client_credentials")
+                    .scopes("write", "read");
     }
 
     @Override
