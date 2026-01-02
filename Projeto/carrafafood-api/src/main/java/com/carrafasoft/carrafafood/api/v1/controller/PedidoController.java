057407d79390b384d0a9e397d25e936efbc5717e
LCarrafaBR <lcarrafa.br@gmail.com>
2026-01-02 14:20:24 -0300

    Desafio: restringindo acessos ao endpoint de pesquisa de pedidos

--- Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/v1/controller/PedidoController.java
+++ Projeto/carrafafood-api/src/main/java/com/carrafasoft/carrafafood/api/v1/controller/PedidoController.java
@@ -82,6 +82,7 @@ public class PedidoController implements PedidoControllerOpenApi {
             @ApiImplicitParam(value = "Nomes das propriedades para filtrar na resposta, separados por vírgula",
             name = "campos", paramType = "query", type = "string")
     })
+    @CheckSecurity.Pedidos.PodePesquisar
     @Override
     @GetMapping
     public PagedModel<PedidoResumoModel> pesquisar(PedidoFilter filtro,
